81d2d3baa3a100fe91f81d30c03c6ffb05601f5b
vanessaokumu <okumuvanessa@gmail.com>
2023-01-24 16:53:47 -0800

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -11,7 +11,7 @@ void cocktail_sort_list(listint_t **list);
  * @tail: pointer to the tail of the doubly-linked list of integers
  * @shaker: pointer to the current swapping node of the cocktail shaker algo
  */
-void swap_node_ahead(listint_t **list, listint_t **tail, lstint_t **shaker)
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 {
 	listint_t *tmp = (*shaker)->next;
 
